9b64b6f43641ab847729dcd46f20046ed8a62c4d
EtB-1 <82047486+EternalB-1@users.noreply.github.com>
2021-04-16 08:48:23 +0700

    Update send.py

--- send.py
+++ send.py
@@ -8,15 +8,15 @@ from rpi_rf import RFDevice
 logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                     format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s', )
 
-parser = argparse.ArgumentParser(description='Sends a decimal code via a 433/315MHz GPIO device')
+parser = argparse.ArgumentParser(description='Отправка десятичных кодов через устройство GPIO 433/315 МГц')
 parser.add_argument('code', metavar='CODE', type=int,
-                    help="Decimal code to send")
+                    help="Десятичный код для отправки")
 parser.add_argument('-g', dest='gpio', type=int, default=21,
-                    help="GPIO pin (default: 21)")
+                    help="GPIO порт (по умолчанию: 21)")
 parser.add_argument('-p', dest='pulselength', type=int, default=None,
-                    help="Pulselength (default: 350)")
+                    help="Длина импульса (по умолчанию: 350)")
 parser.add_argument('-t', dest='protocol', type=int, default=None,
-                    help="Protocol (default: 1)")
+                    help="Протокол (по умолчанию: 1)")
 args = parser.parse_args()
 
 rfdevice = RFDevice(args.gpio)
@@ -31,8 +31,8 @@ if args.pulselength:
 else:
     pulselength = "default"
 logging.info(str(args.code) +
-             " [protocol " + str(protocol) +
-             ", pulselength " + str(pulselength) + "]")
+             " [протокол " + str(protocol) +
+             ", длина импульса " + str(pulselength) + "]")
 
 rfdevice.tx_code(args.code, args.protocol, args.pulselength)
 rfdevice.cleanup()
